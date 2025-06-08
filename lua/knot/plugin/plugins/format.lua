9e355000167f02fd1d6579ad549f888726aea408
arjablc <arjablc@gmail.com>
2025-06-08 15:01:10 +0545

    feat: avante ai code

--- lua/knot/plugin/plugins/format.lua
+++ lua/knot/plugin/plugins/format.lua
@@ -45,6 +45,7 @@ return { -- Autoformat
       graphql = { 'prettierd' },
       liquid = { 'prettierd' },
       python = { 'black', 'isort' },
+      kotlin = { 'ktfmt' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
